c9ccc60d023337e9335cd8c4d6384f88bc223a8b
baayso <baayso@qq.com>
2016-12-25 20:33:37 +0800

    Update hello demo.

--- static/js/hello.js
+++ static/js/hello.js
@@ -1,13 +1,29 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!' + new Date(),
+        message: 'hello vue.js!',
         seen: true,
         todos: [
             {text: 'Learn JavaScript'},
             {text: 'Learn Vue'},
-            {text: 'Build something awesome'}
+            {text: new Date().toLocaleString()}
         ]
+    },
+    filters: {
+        capitalize: function (value) {
+            if (!value) {
+                return '';
+            }
+            value = value.toString();
+            return value.charAt(0).toUpperCase() + value.slice(1);
+        },
+        filterA: function (value, arg1, arg2) {
+            if (!value) {
+                return '';
+            }
+            value = value.toString();
+            return value + arg1 + arg2;
+        }
     },
     methods: {
         change: function () {
@@ -18,6 +34,13 @@ var app = new Vue({
         },
         reverseMessage: function () {
             this.message = this.message.split('').reverse().join('')
+        }
+    },
+    computed: {
+        // a computed getter
+        reversedMessage: function () {
+            // `this` points to the vm instance
+            return this.message.split('').reverse().join('')
         }
     }
 });
